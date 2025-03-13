0f0f175c477397f790ead5868064fe8be6c95180
SwethaParigala06 <parigalaswetha74@gmail.com>
2025-03-13 12:47:56 +0530

    message

---
+++ CharAt.java
@@ -0,0 +1,12 @@
+//Author: Parigala Swetha
+//Date: 03/02/2025
+package Day3;
+
+public class CharAt {
+	public static void main(String args[])
+	{
+		String input="Good Afternoon";
+		char result=input.charAt(5);
+		System.out.println(result);
+	}
+}
